d03a6a1845b10683d5ea12abb2c992126e3e3b4e
Yuri Sosiak <ysosiak@users.noreply.github.com>
2023-12-03 13:04:12 -0700

    sheet view and other misc improvements (#3)
    * sheet view and other misc improvements
    * build-production

--- src/content.ts
+++ src/content.ts
@@ -1,7 +1,8 @@
 const CLICK_DELAY_MS = 70;
 const KEYBOARD_SHORTCUT_ATTRIBUTE = 'data-keyboardshortcut';
+const SHEET_VIEW_ID = 'sheet-view';
 
-const sleep = async(ms: number) => new Promise<void>(resolve => setTimeout(resolve, ms));
+const sleep = async (ms: number) => new Promise<void>(resolve => setTimeout(resolve, ms));
 
 const animateButtonClick = async (
   button: HTMLElement,
@@ -15,49 +16,103 @@ const handleMainControlClick = (
   mainControls: HTMLElement,
   buttonToClick: HTMLElement,
   event: KeyboardEvent,
-): void => {
+): boolean => {
   const modeTabBar = buttonToClick.closest<HTMLElement>('.mode-tab-bar');
   // skip if the element is not visible
   if (!(mainControls.style.opacity !== '0') ||
     modeTabBar != null && !modeTabBar.classList.contains('visible')) {
     console.log(`ignoring ${event.key} due to element visibility`);
-    return;
+    return false;
   }
 
   buttonToClick.click();
+  return true;
 }
 
-const handleButtonClick = (buttonToClick: HTMLElement, event: KeyboardEvent): void => {
-  if (buttonToClick.classList.contains('hidden') || buttonToClick.closest('.hidden') != null) {
-    console.log(`ignoring ${event.key} due to button visibility`);
-    return;
+const handleButtonClick = (buttonToClick: HTMLElement, event: KeyboardEvent): boolean => {
+  const mainControls = buttonToClick.closest<HTMLDivElement>('.main-controls')
+  if (mainControls != null) {
+    return handleMainControlClick(mainControls, buttonToClick, event);
+  } else {
+    if (buttonToClick.classList.contains('hidden') || buttonToClick.closest('.hidden') != null) {
+      console.log(`ignoring ${event.key} due to button visibility`);
+      return false;
+    }
+
+    animateButtonClick(buttonToClick).then(() => buttonToClick.click())
+
+    return true;
   }
-
-  animateButtonClick(buttonToClick)
-    .then(() => buttonToClick.click())
 }
 
-document.addEventListener('keyup', async (event) => {
+const toggleSheetView = (): boolean => {
+  const existingSheetView = document.getElementById(SHEET_VIEW_ID);
+  if (existingSheetView != null) {
+    existingSheetView.remove();
+    return true;
+  } else {
+    const sheetImages = document.querySelectorAll<HTMLElement>('.scrollable-sheet div.sheet-image');
+    if (sheetImages.length === 0) {
+      console.warn('cannot find sheet images');
+      return false;
+    }
+
+    // Create sheet view
+    const sheetView = document.createElement('div');
+    sheetView.id = SHEET_VIEW_ID;
+    // Add close button
+    const closeButton = document.createElement('div');
+    closeButton.classList.add('close-button');
+    closeButton.addEventListener('click', () => sheetView.remove());
+    assignKeyShortcut(closeButton, 'ESCAPE');
+    sheetView.appendChild(closeButton);
+
+    // Add images
+    sheetImages.forEach((image) => {
+      const imageUrlMatch = image.computedStyleMap().get('background-image')?.toString().match(/(https:\/\/.*)"\)/);
+      const imageUrl = imageUrlMatch?.[1];
+      if (imageUrl != null) {
+        const sheetImage = document.createElement('img');
+        sheetImage.classList.add('sheet-image');
+        sheetImage.src = imageUrl;
+        sheetImage.style.maxWidth = `${image.offsetWidth}px`;
+        sheetImage.style.maxHeight = `${image.offsetHeight}px`;
+        sheetView.appendChild(sheetImage);
+      } else {
+        console.warn('cannot find image url', image);
+      }
+    });
+
+    document.body.appendChild(sheetView);
+    return true;
+  }
+}
+
+document.addEventListener('keyup', async (event): Promise<void> => {
   if (event.target instanceof HTMLInputElement) {
     // skip executing hotkeys when inputting text
     return;
   }
+
   const keyPressed = event.key.toUpperCase();
-  const buttonToClick = document.querySelector<HTMLElement>(`[${KEYBOARD_SHORTCUT_ATTRIBUTE}="${keyPressed}"]:not(.hidden)`);
-  if (buttonToClick != null) {
-    const mainControls = buttonToClick.closest<HTMLDivElement>('.main-controls');
-    if (mainControls != null) {
-      handleMainControlClick(mainControls, buttonToClick, event);
-    } else {
-      handleButtonClick(buttonToClick, event);
+  const buttonCandidates = document.querySelectorAll<HTMLElement>(`[${KEYBOARD_SHORTCUT_ATTRIBUTE}="${keyPressed}"]:not(.hidden)`);
+  for (const buttonToClick of buttonCandidates) {
+    if (handleButtonClick(buttonToClick, event)) {
+      // terminate if clicked
+      return;
     }
+  }
+
+  if (event.altKey && keyPressed === 'P' && toggleSheetView()) {
+    // terminate if action is performed
+    return;
   }
 });
 
 const assignKeyShortcut = (element: HTMLElement, key: string) => {
   const existingElementWithHotkey = document.querySelector(`[${KEYBOARD_SHORTCUT_ATTRIBUTE}="${key}"]`);
   if (existingElementWithHotkey != null && existingElementWithHotkey !== element) {
-    console.warn(`conflict when assigning a keyboard shortcut: "${key}"`);
+    console.info(`conflict when assigning a keyboard shortcut: "${key}"`, element);
   }
   element.setAttribute(KEYBOARD_SHORTCUT_ATTRIBUTE, key);
   element.setAttribute('title', `Or press '${key}' on your keyboard`);
@@ -102,7 +157,7 @@ const assignButtonKeyboardShortcut = (button: HTMLButtonElement | HTMLDivElement
   }
 }
 
-const observer = new MutationObserver((mutationList, _) => {
+const observer = new MutationObserver((mutationList: MutationRecord[], _: MutationObserver) => {
   for (const mutation of mutationList) {
     if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
       for (const addedNode of mutation.addedNodes) {
@@ -111,7 +166,9 @@ const observer = new MutationObserver((mutationList, _) => {
             assignMainControlsKeyboardShortcuts(addedNode);
           } else if ([...addedNode.classList.values()].some(className => className.startsWith('css-'))) {
             addedNode.querySelectorAll<HTMLButtonElement>('button[data-testid]').forEach((button) => {
-              assignButtonKeyboardShortcut(button);
+              if (button.getAttribute('data-testid')?.endsWith('Button')) {
+                assignButtonKeyboardShortcut(button);
+              }
             });
           } else if (addedNode.classList.contains('learnstep-notification') || addedNode.classList.contains('centered-children')) {
             addedNode.querySelectorAll<HTMLDivElement>('.buttons > div.button').forEach((button) => {
